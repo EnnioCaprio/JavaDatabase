ea2f2ceee5320add705b32df3f811101e604f584
Ennio Caprio <ennio520@gmail.com>
2019-04-06 11:07:09 +0200

    Translated DB

--- Db/src/controller/EseSoft.java
+++ Db/src/controller/EseSoft.java
@@ -9,51 +9,58 @@ public class EseSoft {
 
 	public static void main(String[] args) throws SQLException{
 
-		Scanner input = new Scanner(System.in); 
-		
+		Scanner input = new Scanner(System.in);
+
 		boolean lights = true;
 
 		MaskOutput m = new MaskOutput();
 
 		String aggiungi;
-		
+
 		CRUD c = new CRUD();
-		
+
 		Giochi g = new Giochi();
 
 		while(lights == true){
 			System.out.println("Scegli cosa fare: 1 - Aggiungi Prodotto, 2 - Aggiorna Prodotto, 3 - Elimina Prodotto, 4 - Elimina tutti i prodotti, 5 - Leggi");
+			System.out.println("Choose what do you want to do: 1- Add Product, 2 - Update Product, 3 - Delete Product, 4 Delete all the products, 5 - Read");
 			int scelta = input.nextInt();
 			switch(scelta) {
 			case 1: m.aggiungi(g);
 			c.inserisciProdotto(g);
 			System.out.println("Inserito!");
+			System.out.println("Added");
 			break;
 			case 2: m.aggiorna(g);
 			c.aggiornaProdotto(g);
 			System.out.println("Aggiornato!");
+			System.out.println("Updated");
 			break;
 			case 3: m.elimina(g);
 			c.eliminaProdotto(g);
 			System.out.println("Eliminato!");
+			System.out.println("Deleted");
 			break;
 			case 4: m.eliminaTutto(g);
 			System.out.println("Eliminati tutti i prodotti!");
+			System.out.println("Deleted all the products!");
 			break;
 			case 5: m.leggi(g);
 			c.leggiProdotto(g);
 			break;
 			default: System.out.println("Non corretto");
+							 System.out.println("Not right");
 			break;
 			}
-			System.out.println("Vuoi scegliere altre funzionalità? si o no");
+			System.out.println("Vuoi scegliere altre funzionalitï¿½? si o no");
+			System.out.println("Do you want to use other functions? si or no");
 			aggiungi = input.next();
 			if(aggiungi.equals("si")) {
 				lights = true;
 			}else if(aggiungi.equals("no")) {
 				lights = false;
 			}else {
-				System.out.println("Scelta non corretta");
+				System.out.println("Scelta non corretta/Wrong");
 				lights = false;
 			}
 		}
--- Db/src/view/MaskOutput.java
+++ Db/src/view/MaskOutput.java
@@ -6,62 +6,62 @@ import model.Giochi;
 public class MaskOutput {
 	Scanner input = new Scanner(System.in);
 	public void aggiungi(Giochi g) {
-		System.out.println("Aggiungi gioco");
+		System.out.println("Aggiungi gioco/Add game");
 		g.setGioco(input.nextLine());
-		System.out.println("Aggiungi il prezzo gioco");
+		System.out.println("Aggiungi il prezzo gioco/Add game's price");
 		g.setPrezzo(Double.parseDouble(input.nextLine()));
 	}
 
 	public void aggiorna(Giochi g) throws SQLException {
 		CRUD c = new CRUD();
 		c.leggiProdotto(g);
-		System.out.println("Sei sicuro di voler aggiornare un prodotto? si or no");
+		System.out.println("Sei sicuro di voler aggiornare un prodotto? si or no/Are you sure that you want to update your product? si(yes) or no(no)");
 		String scelta = input.nextLine();
 		if(scelta.equals("si")) {
-			System.out.println("Aggiorna nome gioco");
+			System.out.println("Aggiorna nome gioco/Update game's name");
 			g.setGioco(input.nextLine());
-			System.out.println("Aggiorna il prezzo");
+			System.out.println("Aggiorna il prezzo/Update game's price");
 			g.setPrezzo(Double.parseDouble(input.nextLine()));
-			System.out.println("Scegli gioco");
+			System.out.println("Scegli gioco/Choose the game by Id number");
 			g.setId(Integer.parseInt(input.nextLine()));
 		}else if(scelta.equals("no")) {
-			System.out.println("Processo terminato");
+			System.out.println("Processo terminato/Stop");
 		}else {
-			System.out.println("Tasto sbagliato o combinazione di tasti sbagliati");
+			System.out.println("Tasto sbagliato o combinazione di tasti sbagliati/You typed wrong number or wrong letter");
 		}
 	}
 
 	public void elimina(Giochi g) throws SQLException {
 		CRUD c = new CRUD();
 		c.leggiProdotto(g);
-		System.out.println("Sei sicuro di voler eliminare un prodotto? si or no");
+		System.out.println("Sei sicuro di voler eliminare un prodotto? si or no/Are you sure that you want to eliminate your product? si(yes) or no(no)");
 		String scelta = input.nextLine();
 		if(scelta.equals("si")) {
-			System.out.println("Elimina gioco");
+			System.out.println("Elimina gioco/Eliminate game");
 			g.setGioco(input.nextLine());
 		}else if(scelta.equals("no")) {
-			System.out.println("Processo terminato");
+			System.out.println("Processo terminato/Stop");
 		}else {
-			System.out.println("Tasto sbagliato");
+			System.out.println("Tasto sbagliato o combinazione di tasti sbagliati/You typed wrong number or wrong letter");
 		}
 	}
-	
+
 	public void eliminaTutto(Giochi g) throws SQLException {
 		CRUD c = new CRUD();
 		c.leggiProdotto(g);
-		System.out.println("Sei sicuro di voler eliminare tutti i prodotti? si or no");
+		System.out.println("Sei sicuro di voler eliminare tutti i prodotti? si or no/Are you sure that you want to eliminate all your products? si(yes) or no(no)");
 		String scelta = input.nextLine();
 		if(scelta.equals("si")) {
 			c.eliminaProdottiEPulisci(g);
 		}else if(scelta.equals("no")) {
-			System.out.println("Processo terminato");
+			System.out.println("Processo terminato/Stop");
 		}else {
-			System.out.println("Tasto sbagliato");
+			System.out.println("Tasto sbagliato o combinazione di tasti sbagliati/You typed wrong number or wrong letter");
 		}
 	}
-	
+
 	public void leggi(Giochi g) {
-		System.out.println("Giochi presenti");
+		System.out.println("Giochi presenti/Games available");
 		System.out.println("ID -- Prodotto -- Prezzo");
 	}
 }
